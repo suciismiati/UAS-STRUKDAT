80cafda5e64b5a45e791bc2fb61d4596b83f65fb
VolemAlvaro <120910442+VolemAlvaro@users.noreply.github.com>
2022-12-27 20:57:44 +0700

    Update UAS_STRUKDAT_KEL4.cpp

--- UAS_STRUKDAT_KEL4.cpp
+++ UAS_STRUKDAT_KEL4.cpp
@@ -215,6 +215,7 @@ int main()
   printf("21081010003 - Volem Alvaro Azira\n");
   printf("20081010239 - Rifqi Alvian Ardhiansyah\n");
   printf("20081010167 - Candra Kusuma Muhammad Bimantara\n");
+  printf("20081018261 - Ali Muhammad Saleh Baaboud\n");
   printf("20081010162 - Suci Ismiati\n\n");
   
   PackageQueue *queue = create_queue();
